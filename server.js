567cfe21e5c2ed9bb60bc0c4a57d3e21b565c518
Manuel <mgrivas777@gmail.com>
2019-08-31 13:49:59 -0700

    Edited .gitignore to include config file. Added new packages

--- server.js
+++ server.js
@@ -2,18 +2,15 @@
 
 const express = require('express');
 const app = express();
+const chatan = require('./app');
+
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
-
-app.get('/', (req, res, next) => {
-	res.render('login');
-});
-
+app.use('/', chatan.router);
 
 
 app.listen(app.get('port'), () => {
-	console.log("Running on port 3000...");
+	console.log("Running on port " + app.get('port') + "...");
 });
 
